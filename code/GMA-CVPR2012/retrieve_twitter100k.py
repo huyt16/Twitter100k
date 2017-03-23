aa9e07b010fc43aa24c3aa57b4a6d81913ada14c
huyt16 <740733663@qq.com>
2017-03-23 18:28:24 +0800

    Update retrieve_twitter100k.py

--- code/GMA-CVPR2012/retrieve_twitter100k.py
+++ code/GMA-CVPR2012/retrieve_twitter100k.py
@@ -5,7 +5,7 @@ import os
 
 print 'load data! Wait please!'
 dataset='twitter100k'
-root='../../feature/'+dataset'
+root='../../feature/'+dataset
 image=sio.loadmat(root+'/test_image.mat')['image']
 text=sio.loadmat(root+'/lda.mat')['text']
 #text=sio.loadmat(root+'/text_word2vec_bow.mat')['text']
